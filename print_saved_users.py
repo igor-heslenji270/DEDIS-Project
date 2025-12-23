f35178ded51850992074139a469cf76ca989a007
igor-heslenji270 <89252108@student.upr.si>
2025-12-23 14:01:44 +0100

    Remove unnecessary files from repository

--- print_saved_users.py
+++
@@ -1,8 +0,0 @@
-from app.database import SessionLocal
-from app import models
-
-db = SessionLocal()
-users = db.query(models.User).all()
-for u in users:
-    print(u.id, u.username, u.hashed_password, u.salt)
-db.close()
